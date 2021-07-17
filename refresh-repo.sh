603bcb98492812d6b82ce47215efde700ca4b8bb
Tamal Saha <tamal@appscode.com>
2021-07-17 03:28:41 -0700

    Update refresh-repo.sh
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- refresh-repo.sh
+++ refresh-repo.sh
@@ -20,7 +20,7 @@ refresh() {
     git checkout -b $PR_BRANCH
     go env -w GOPRIVATE=kubeform.dev/*
     go mod edit \
-        -require=kubeform.dev/generator-v2@v0.0.8
+        -require=kubeform.dev/generator-v2@v0.0.9
     go mod tidy
     go mod vendor
     [ -z "$2" ] || (
