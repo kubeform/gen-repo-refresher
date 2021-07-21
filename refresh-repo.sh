b28e612a28ce4d718bd1a1d68e50836ccd6cb91d
Tamal Saha <tamal@appscode.com>
2021-07-21 11:26:59 -0700

    Use v0.0.11
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- refresh-repo.sh
+++ refresh-repo.sh
@@ -20,7 +20,7 @@ refresh() {
     git checkout -b $PR_BRANCH
     go env -w GOPRIVATE=kubeform.dev/*
     go mod edit \
-        -require=kubeform.dev/generator-v2@v0.0.10
+        -require=kubeform.dev/generator-v2@v0.0.11
     go mod tidy
     go mod vendor
     [ -z "$2" ] || (
