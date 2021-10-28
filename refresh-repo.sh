3f3e3374dfa2514ac516f21b1e827bb56567db10
Mohammad Fahim Abrar <fahimabrar@appscode.com>
2021-10-28 19:42:39 +0600

    Add provider versions in `repos.txt`
    Signed-off-by: Mohammad Fahim Abrar <fahimabrar@appscode.com>

--- refresh-repo.sh
+++ refresh-repo.sh
@@ -28,27 +28,34 @@ refresh() {
         go mod edit \
             -require=kubeform.dev/generator-v2@v0.0.25
     fi
+
+    if [[ $2 == *"="* ]]; then
+        go mod edit -replace=$2
+    else
+        go mod edit -require=$2
+    fi
+
     go mod tidy
     go mod vendor
-    sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/azure-sdk-for-go \\|g' hack/scripts/generate.sh
-    sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-autorest \\|g' hack/scripts/generate.sh
-    sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-autorest/autorest \\|g' hack/scripts/generate.sh
-    sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-autorest/autorest/adal \\|g' hack/scripts/generate.sh
-    sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-autorest/autorest/date \\|g' hack/scripts/generate.sh
-    sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-autorest/autorest/mocks \\|g' hack/scripts/generate.sh
-    sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-autorest/autorest/to \\|g' hack/scripts/generate.sh
-    sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-autorest/autorest/validation \\|g' hack/scripts/generate.sh
-    sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-autorest/logger \\|g' hack/scripts/generate.sh
-    sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-autorest/tracing \\|g' hack/scripts/generate.sh
-    sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-ansiterm \\|g' hack/scripts/generate.sh
-    sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -require=kubeform.dev/terraform-backend-sdk@v0.0.0-20210922115523-21574335f0db \\|g' hack/scripts/generate.sh
-    sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -dropreplace=google.golang.org/api \\|g' hack/scripts/generate.sh
+    # sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/azure-sdk-for-go \\|g' hack/scripts/generate.sh
+    # sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-autorest \\|g' hack/scripts/generate.sh
+    # sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-autorest/autorest \\|g' hack/scripts/generate.sh
+    # sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-autorest/autorest/adal \\|g' hack/scripts/generate.sh
+    # sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-autorest/autorest/date \\|g' hack/scripts/generate.sh
+    # sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-autorest/autorest/mocks \\|g' hack/scripts/generate.sh
+    # sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-autorest/autorest/to \\|g' hack/scripts/generate.sh
+    # sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-autorest/autorest/validation \\|g' hack/scripts/generate.sh
+    # sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-autorest/logger \\|g' hack/scripts/generate.sh
+    # sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-autorest/tracing \\|g' hack/scripts/generate.sh
+    # sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -dropreplace=github.com/Azure/go-ansiterm \\|g' hack/scripts/generate.sh
+    # sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\\n    -require=kubeform.dev/terraform-backend-sdk@v0.0.0-20210922115523-21574335f0db \\|g' hack/scripts/generate.sh
+    # sed -i 'N;s|go mod edit \\\n    -replace=google.golang.org/api=google.golang.org/api@v0.59.0 \\|go mod edit \\\n    -dropreplace=google.golang.org/api \\|g' hack/scripts/generate.sh
     sed -i 's|kmodules.xyz/client-go@13d22e91512b80f1ac6cbb4452c3be73e7a21b88|kmodules.xyz/client-go@5e9cebbf1dfa80943ecb52b43686b48ba5df8363|g' hack/scripts/generate.sh
     sed -i 's|kubeform.dev/apimachinery@7bcd34a30eb5956ae85815ea522e58b0c85db48e|kubeform.dev/apimachinery@ba5604d5a1ccd6ea2c07c6457c8b03f11ab00f63|g' hack/scripts/generate.sh
-    [ -z "$2" ] || (
-        echo "$2"
-        $2 || true
-    )
+    # [ -z "$2" ] || (
+    #     echo "$2"
+    #     $2 || true
+    # )
     git add --all
     if git diff --exit-code -s HEAD; then
         echo "Repository $1 is up-to-date."
@@ -75,7 +82,7 @@ if [ -x $GITHUB_TOKEN ]; then
 fi
 
 # ref: https://linuxize.com/post/how-to-read-a-file-line-by-line-in-bash/#using-file-descriptor
-while IFS=, read -r -u9 repo cmd; do
+while IFS=' ' read -r -u9 repo cmd; do
     if [ -z "$repo" ]; then
         continue
     fi
