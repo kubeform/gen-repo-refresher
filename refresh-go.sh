c9df4668f19113c0fbf47a6af50f0c4a0edd720e
Tamal Saha <tamal@appscode.com>
2022-05-07 22:34:36 -0700

    Add script to update go mod
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ refresh-go.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# set -eou pipefail
+
+SCRIPT_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
+
+GITHUB_USER=${GITHUB_USER:-1gtm}
+PR_BRANCH=gomod-refresher # -$(date +%s)
+COMMIT_MSG="Use Go mod 1.17"
+
+REPO_ROOT=/tmp/gen-repo-refresher
+
+repo_uptodate() {
+    # gomodfiles=(go.mod go.sum vendor/modules.txt)
+    gomodfiles=(go.sum vendor/modules.txt)
+    changed=($(git diff --name-only))
+    changed+=("${gomodfiles[@]}")
+    # https://stackoverflow.com/a/28161520
+    diff=($(echo ${changed[@]} ${gomodfiles[@]} | tr ' ' '\n' | sort | uniq -u))
+    return ${#diff[@]}
+}
+
+refresh() {
+    echo "refreshing repository: $1"
+    rm -rf $REPO_ROOT
+    mkdir -p $REPO_ROOT
+    pushd $REPO_ROOT
+    git clone --no-tags --no-recurse-submodules --depth=1 https://${GITHUB_USER}:${GITHUB_TOKEN}@$1.git
+    cd $(ls -b1)
+    git checkout -b $PR_BRANCH
+    if [ -f go.mod ]; then
+        sed -i "s|go 1.12|go 1.17|g" go.mod
+        sed -i "s|go 1.13|go 1.17|g" go.mod
+        sed -i "s|go 1.14|go 1.17|g" go.mod
+        sed -i "s|go 1.15|go 1.17|g" go.mod
+        sed -i "s|go 1.16|go 1.17|g" go.mod
+        go get github.com/modern-go/reflect2@v1.0.2
+        go get github.com/json-iterator/go@v1.1.12
+        go mod tidy
+        go mod vendor
+    fi
+    [ -z "$2" ] || (
+        echo "$2"
+        $2 || true
+        # always run make fmt incase make gen fmt fails
+        make fmt || true
+    )
+    if repo_uptodate; then
+        echo "Repository $1 is up-to-date."
+    else
+        git add --all
+        if [[ "$1" == *"stashed"* ]]; then
+            git commit -a -s -m "$COMMIT_MSG" -m "/cherry-pick"
+        else
+            git commit -a -s -m "$COMMIT_MSG"
+        fi
+        git push -u origin $PR_BRANCH -f
+        hub pull-request \
+            --labels automerge \
+            --message "$COMMIT_MSG" \
+            --message "Signed-off-by: $(git config --get user.name) <$(git config --get user.email)>" || true
+        # gh pr create \
+        #     --base master \
+        #     --fill \
+        #     --label automerge \
+        #     --reviewer tamalsaha
+    fi
+    popd
+}
+
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters"
+    echo "Correct usage: $SCRIPT_NAME <path_to_repos_list>"
+    exit 1
+fi
+
+if [ -x $GITHUB_TOKEN ]; then
+    echo "Missing env variable GITHUB_TOKEN"
+    exit 1
+fi
+
+# ref: https://linuxize.com/post/how-to-read-a-file-line-by-line-in-bash/#using-file-descriptor
+while IFS=' ' read -r -u9 repo cmd; do
+    if [ -z "$repo" ]; then
+        continue
+    fi
+    refresh "$repo" "$cmd"
+    echo "################################################################################"
+done 9<$1
